31245dd7ab069c34aa93fc7f931b5387b0a939ff
farseen <farseen247@gmail.com>
2025-10-23 23:20:07 +0530

    process,env

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
+const { CLIENT_URL } = require('./config/env');
+
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
--- server/config/env.js
+++ server/config/env.js
@@ -3,4 +3,5 @@ require('dotenv').config();
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   PORT: process.env.PORT || 5000,
+  CLIENT_URL: process.env.CLIENT_URL,
 };
