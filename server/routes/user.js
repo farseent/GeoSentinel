d45b7fe207b7197028da4993bc8fce5d4315c358
farseen <farseen247@gmail.com>
2025-10-26 13:10:35 +0530

    authorize role - User

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
+const authorizeRoles = require('../middleware/authorizeRoles');
 
-router.get('/profile', auth, userController.getUserProfile);
-router.put('/profile', auth, userController.updateUserProfile);
-router.get('/requests', auth, userController.getUserRequests);
-router.get('/stats', auth, userController.getUserStats);
+
+router.get('/profile', auth, authorizeRoles('user'), userController.getUserProfile);
+router.put('/profile', auth, authorizeRoles('user'), userController.updateUserProfile);
+router.get('/requests', auth, authorizeRoles('user'), userController.getUserRequests);
+router.get('/stats', auth, authorizeRoles('user'), userController.getUserStats);
 
 module.exports = router;
